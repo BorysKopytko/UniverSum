439ea23f23a626f92322b828705e4cd41b934288
Borys Kopytko <kopytkoborys@gmail.com>
2024-11-11 13:15:42 +0200

    Install CommunityToolkit.Maui and clean VerticalStackLayouts

--- UniverSum.Client/MauiProgram.cs
+++ UniverSum.Client/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace UniverSum.Client
 {
@@ -9,6 +10,7 @@ namespace UniverSum.Client
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
